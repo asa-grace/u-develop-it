59261ca6511104c59161cff92e20c74f115bfdb1
Asa Grace <asag182@gmail.com>
2021-07-17 17:49:48 -0600

    Parties and candidates CRUD

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ app.get('/api/party/:id', (req, res) => {
 
 // Update a candidates party
 app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
   const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
   const params = [req.body.party_id, req.params.id];
   db.query(sql, params, (err, result) => {
