ea090de1011c797e2580f078535c4e33dba12c53
Asa Grace <asag182@gmail.com>
2021-07-17 15:03:29 -0600

    add connection file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
 const app = express();
 
 // Express middleware
